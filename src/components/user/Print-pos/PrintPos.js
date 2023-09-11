66de8a46ea66c525ad056a50675fe0a5194f29f0
riteshbhuwad <ritesh@powerlook.in>
2023-09-12 01:16:09 +0530

    Print code added

---
+++ src/components/user/Print-pos/PrintPos.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import escpos from 'escpos';
+
+const device = new escpos.USB();
+const printer = new escpos.Printer(device);
+
+const PrintPos = () => {
+  const data = [
+    { heading: 'betclose', details: 1694460843.089 },
+    { heading: 'creditPoint', details: 4980 },
+    { heading: 'date', details: '2023-09-11 07:33:08' },
+    // Add more data pairs as needed
+  ];
+
+  const printData = () => {
+    try {
+      device.open(() => {
+        printer
+          .font('A')
+          .align('lt') // Left align the text
+          .style('bu')
+          .size(1, 1);
+
+        // Loop through the data array and print each pair
+        data.forEach((item) => {
+          printer.text(`${item.heading} : ${item.details}`);
+        });
+
+        printer.cut().close();
+      });
+    } catch (error) {
+      console.error('Error printing:', error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={printData}>Print Data</button>
+    </div>
+  );
+}
+
+export default PrintPos;
